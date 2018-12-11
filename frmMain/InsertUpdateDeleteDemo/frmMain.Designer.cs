fb908e650d5c87bc67cd1e562a95b4991a7f4efa
Hakan Ayata <hakanayata01@hotmail.com>
2018-12-11 13:41:23 +0300

    frmMain

---
+++ frmMain/InsertUpdateDeleteDemo/frmMain.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace InsertUpdateDeleteDemo
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_Name = new System.Windows.Forms.TextBox();
+            this.txt_State = new System.Windows.Forms.TextBox();
+            this.btn_Insert = new System.Windows.Forms.Button();
+            this.btn_Update = new System.Windows.Forms.Button();
+            this.btn_Delete = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txt_TcNo = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(43, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(43, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "State";
+            // 
+            // txt_Name
+            // 
+            this.txt_Name.Location = new System.Drawing.Point(127, 64);
+            this.txt_Name.Name = "txt_Name";
+            this.txt_Name.Size = new System.Drawing.Size(129, 20);
+            this.txt_Name.TabIndex = 2;
+            // 
+            // txt_State
+            // 
+            this.txt_State.Location = new System.Drawing.Point(127, 97);
+            this.txt_State.Name = "txt_State";
+            this.txt_State.Size = new System.Drawing.Size(129, 20);
+            this.txt_State.TabIndex = 3;
+            // 
+            // btn_Insert
+            // 
+            this.btn_Insert.Location = new System.Drawing.Point(127, 148);
+            this.btn_Insert.Name = "btn_Insert";
+            this.btn_Insert.Size = new System.Drawing.Size(75, 23);
+            this.btn_Insert.TabIndex = 4;
+            this.btn_Insert.Text = "Insert";
+            this.btn_Insert.UseVisualStyleBackColor = true;
+            this.btn_Insert.Click += new System.EventHandler(this.btn_Insert_Click);
+            // 
+            // btn_Update
+            // 
+            this.btn_Update.Location = new System.Drawing.Point(208, 148);
+            this.btn_Update.Name = "btn_Update";
+            this.btn_Update.Size = new System.Drawing.Size(75, 23);
+            this.btn_Update.TabIndex = 5;
+            this.btn_Update.Text = "Update";
+            this.btn_Update.UseVisualStyleBackColor = true;
+            this.btn_Update.Click += new System.EventHandler(this.btn_Update_Click);
+            // 
+            // btn_Delete
+            // 
+            this.btn_Delete.Location = new System.Drawing.Point(289, 148);
+            this.btn_Delete.Name = "btn_Delete";
+            this.btn_Delete.Size = new System.Drawing.Size(75, 23);
+            this.btn_Delete.TabIndex = 6;
+            this.btn_Delete.Text = "Delete";
+            this.btn_Delete.UseVisualStyleBackColor = true;
+            this.btn_Delete.Click += new System.EventHandler(this.btn_Delete_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(46, 201);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(461, 150);
+            this.dataGridView1.TabIndex = 7;
+            this.dataGridView1.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_RowHeaderMouseClick);
+            // 
+            // txt_TcNo
+            // 
+            this.txt_TcNo.Location = new System.Drawing.Point(127, 38);
+            this.txt_TcNo.Name = "txt_TcNo";
+            this.txt_TcNo.Size = new System.Drawing.Size(129, 20);
+            this.txt_TcNo.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(43, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Tc No";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(563, 374);
+            this.Controls.Add(this.txt_TcNo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_Delete);
+            this.Controls.Add(this.btn_Update);
+            this.Controls.Add(this.btn_Insert);
+            this.Controls.Add(this.txt_State);
+            this.Controls.Add(this.txt_Name);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmMain";
+            this.Text = "frmMain";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_Name;
+        private System.Windows.Forms.TextBox txt_State;
+        private System.Windows.Forms.Button btn_Insert;
+        private System.Windows.Forms.Button btn_Update;
+        private System.Windows.Forms.Button btn_Delete;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txt_TcNo;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ frmMain/InsertUpdateDeleteDemo/frmMain.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.DataAccess.Client;
+
+namespace InsertUpdateDeleteDemo
+{
+    
+    public partial class frmMain : Form
+    {
+       
+     
+            OracleConnection con = new OracleConnection("Data Source=(DESCRIPTION ="
+                                        + "(ADDRESS = (PROTOCOL = TCP)(HOST = 127.0.0.1)(PORT = 1521))"
+                                        + "(CONNECT_DATA =" + "(SERVER = DEDICATED)"
+                                        + "(SERVICE_NAME = BELSOFT)));"
+                                        + "User Id= SAMPLE;Password=BELSOFT;");
+             OracleCommand cmd;
+             OracleDataAdapter adapt;
+             int ID = 0;
+        public frmMain()
+        {
+            InitializeComponent();
+            DisplayData();
+        }
+        
+        private void btn_Insert_Click(object sender, EventArgs e)
+        {        
+
+            con.Open();
+            if (txt_Name.Text != "" && txt_State.Text != "")
+            {
+                string sSql = "Insert into TBL_RECORD(TCNO,NAME,STATE)" + "Values('" + txt_TcNo.Text + "','" + txt_Name.Text + "','" + txt_State.Text +"')";
+                cmd = new OracleCommand(sSql,con);       
+               try
+                {
+                    cmd.ExecuteNonQuery();  
+                    MessageBox.Show("Record Inserted Successfully");
+                    con.Close();
+                    DisplayData();
+                    ClearData();
+                }
+                catch (Exception exp)
+                {
+                    MessageBox.Show ("Code: " + exp.Source.ToString() + "\n" + "Message: " + exp.Message.ToString() + "\n" + "An exception occurred. Please contact your system administrator.");
+                    con.Close();
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Please Provide Details!");
+                con.Close();
+            }
+        }
+        //Display Data in DataGridView  
+        private void DisplayData()
+        {
+            string sSql = "select * from tbl_Record";
+            con.Open();
+            DataTable dt = new DataTable();
+            adapt = new OracleDataAdapter(sSql, con);
+            adapt.Fill(dt);
+            dataGridView1.DataSource = dt;
+            con.Close();
+        }
+        //Clear Data  
+        private void ClearData()
+        {
+            txt_TcNo.Text = "";
+            txt_Name.Text = "";
+            txt_State.Text = "";
+            ID = 0;
+        }
+        //dataGridView1 RowHeaderMouseClick Event  
+        private void dataGridView1_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            try
+            { 
+            ID = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString());
+            txt_TcNo.Text = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+            txt_Name.Text = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+            txt_State.Text = dataGridView1.Rows[e.RowIndex].Cells[3].Value.ToString();
+            }
+            catch(Exception exp)
+            {
+               // MessageBox.Show("Code: " + exp.Source.ToString() + "\n" + "Message: " + exp.Message.ToString() + "\n" + "An exception occurred. Please contact your system administrator.");
+                con.Close();
+            }
+        }
+
+        private void btn_Update_Click(object sender, EventArgs e)
+        {
+            if (txt_Name.Text != "" && txt_State.Text != "")
+            {
+                con.Open();
+                string sSql = "update tbl_Record set TCNO='" + txt_TcNo.Text +"', NAME= '"+ txt_Name.Text +"', STATE= '"+ txt_State.Text +"' WHERE ID="+ID;
+                cmd = new OracleCommand(sSql, con);
+                   
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Record Updated Successfully");
+                con.Close();
+                DisplayData();
+                ClearData();
+            }
+            else
+            {
+                MessageBox.Show("Please Select Record to Update");
+            }
+        }
+        //Delete Record  
+        private void btn_Delete_Click(object sender, EventArgs e)
+        {
+            if (ID != 0)
+            {
+                con.Open();
+                string sSql = "delete tbl_Record where ID="+ID;
+                cmd = new OracleCommand(sSql, con);
+                
+            
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("Record Deleted Successfully!");
+                DisplayData();
+                ClearData();
+            }
+            else
+            {
+                MessageBox.Show("Please Select Record to Delete");
+            }
+        }
+
+    }
+
+}
